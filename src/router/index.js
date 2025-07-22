4c3f37003d9ce917cad00be268bbd7cb92997518
Анатолий Косолапов <anatolii@MacBook-Air-Anatolij.local>
2025-07-22 18:45:38 +0300

    updated router

--- src/router/index.js
+++ src/router/index.js
@@ -13,9 +13,10 @@ import { useUserStore } from 'stores/user-store'
  */
 
 export default defineRouter(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+  // const createHistory = process.env.SERVER
+  //   ? createMemoryHistory
+  //   : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+  const createHistory = createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
